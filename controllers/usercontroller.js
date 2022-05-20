a5ccaaeb12a4b19ecd252c1e7511f4d8e0f5797f
Jiří Lenner <ji.lenner@gmail.com>
2022-05-20 17:00:25 +0200

    added user route and protected it for authorised users with token only

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,6 +1,7 @@
 const catchAsync = require('../utils/catchasync.js');
 const appError = require('../utils/apperror.js');
 const User = require('../model/usermodel.js');
+const jwt = require('jsonwebtoken');
 
 /**
  * @typedef { import('express').Request } Request
@@ -126,4 +127,116 @@ exports.deleteUser = catchAsync(async (req, res, next) => {
  * @param {NextFunction} next Express next function
  * @description Middleware for authenticating user and returning access token
  */
-exports.login = catchAsync(async (req, res, next) => {});
+exports.login = catchAsync(async (req, res, next) => {
+	const {email, password} = req.body;
+
+	// request
+	if (!email || !password) {
+		return next(
+			new appError('Chybí heslo a email', 400)
+		);
+	}
+
+	// check db whether the user with specified email exists
+	const user = await User.findOne({email});
+
+	if (!user) {
+		return next(
+			new appError(
+				'Incorrect email or password!',
+				401
+			)
+		);
+	}
+
+	// check password
+	if (user.password !== password) {
+		return next(
+			new appError(
+				'Incorrect email or password!',
+				401
+			)
+		);
+	}
+
+	// token content
+	const payload = {
+		name: user.userName,
+		sub: user._id,
+	};
+
+	const secret = process.env.JWT_SECRET;
+	const options = {
+		expiresIn: process.env.JWT_EXPIRATION,
+	};
+
+	// sign token
+	const token = jwt.sign(payload, secret, options);
+
+	// send response with the token
+	res.status(200).json({
+		status: 'success',
+		token,
+	});
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+	// Getting the token + check existence
+	let token;
+
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.startsWith('Bearer')
+	) {
+		token = req.headers.authorization.split(' ')[1];
+	}
+
+	if (!token) {
+		return next(new appError('Nejsi přihlášen!', 401));
+	}
+
+	// Verification
+	const decodedPayload = await jwt.verify(
+		token,
+		process.env.JWT_SECRET
+	);
+
+	// Check if the user still exists
+	const user = await User.findById(decodedPayload.sub);
+
+	if (!user) {
+		return next(
+			new appError(
+				'Uživatel přiřazený k tomuto tokenu již neexistuje!',
+				401
+			)
+		);
+	}
+
+	// protect specifc routes for admin use only!!
+
+	//TODO přepsat na switch
+	if (
+		req.route.path === '/' &&
+		req.method == 'POST' &&
+		!user.admin
+	) {
+		return next(new appError('Nejsi admin!', 401));
+	} else if (
+		req.route.path === '/:id' &&
+		req.method == 'DELETE' &&
+		!user.admin
+	) {
+		return next(new appError('Nejsi admin!', 401));
+	} else if (
+		req.route.path === '/:id' &&
+		req.method == 'PATCH' &&
+		!user.admin
+	) {
+		return next(new appError('Nejsi admin!', 401));
+	}
+
+	// add user data to the request and grant access
+	req.user = user;
+	next();
+});
